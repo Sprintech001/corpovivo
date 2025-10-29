059b472064ab6f0c5ca5f7c277450eb91ae56230
Thomaz Rodrigues <thomazrodrigues668@gmail.com>
2025-10-29 13:51:50 -0300

    fix: update Vercel runtime to Node.js 20

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: vercel({
-			runtime: 'nodejs18.x' // ou 'nodejs20.x'
+			runtime: 'nodejs20.x' // atualizado para compatibilidade com Vercel
 		  })
 	}
 };
